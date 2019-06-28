279456a8c9aa178686e8be5ecf561d9f7ffa500e
Harvey Deng <Harvey.Deng@morningstar.com>
2019-06-28 15:54:43 +0800

    治理编排功能之————配置中心化 自己用代码实现的算法无法中心化

--- src/main/java/com/example/shardingjdbcdemo/config/ShardingConfig.java
+++ src/main/java/com/example/shardingjdbcdemo/config/ShardingConfig.java
@@ -1,15 +1,14 @@
 package com.example.shardingjdbcdemo.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
-import com.example.shardingjdbcdemo.algorithm.MyHintShardingAlgorithm;
 import com.example.shardingjdbcdemo.algorithm.MyStandardShardingAlgorithm;
-import org.apache.shardingsphere.api.config.sharding.KeyGeneratorConfiguration;
-import org.apache.shardingsphere.api.config.sharding.ShardingRuleConfiguration;
-import org.apache.shardingsphere.api.config.sharding.TableRuleConfiguration;
-import org.apache.shardingsphere.api.config.sharding.strategy.HintShardingStrategyConfiguration;
-import org.apache.shardingsphere.api.config.sharding.strategy.InlineShardingStrategyConfiguration;
-import org.apache.shardingsphere.api.config.sharding.strategy.StandardShardingStrategyConfiguration;
-import org.apache.shardingsphere.shardingjdbc.api.ShardingDataSourceFactory;
+import io.shardingsphere.api.config.rule.ShardingRuleConfiguration;
+import io.shardingsphere.api.config.rule.TableRuleConfiguration;
+import io.shardingsphere.api.config.strategy.InlineShardingStrategyConfiguration;
+import io.shardingsphere.api.config.strategy.StandardShardingStrategyConfiguration;
+import io.shardingsphere.orchestration.config.OrchestrationConfiguration;
+import io.shardingsphere.orchestration.reg.api.RegistryCenterConfiguration;
+import io.shardingsphere.shardingjdbc.orchestration.api.OrchestrationShardingDataSourceFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,17 +22,54 @@ import java.util.Properties;
 @Configuration
 public class ShardingConfig {
 
+    // 适用Zookeeper
+    private static final String ZOOKEEPER_CONNECTION_STRING = "localhost:2181";
+
+    private static final String NAMESPACE = "orchestration-java-demo";
+
     @Autowired
     private DatasourceConfig datasourceConfig;
 
+    /**
+     * 这适用于将注册中心上的配置拉取下去覆盖自己的配置（配置中心化功能）
+     * !!!自己写的算法，如分库算法、分表算法无法中心化
+     * @return
+     * @throws SQLException
+     */
+//    @Bean
+//    public DataSource getDataSource() throws SQLException {
+//        return OrchestrationShardingDataSourceFactory.createDataSource(new OrchestrationConfiguration("sharding-sphere-orchestration", getRegistryCenterConfiguration(), false));
+//    }
+
+    /**
+     * 这适用于将local改变的配置覆盖注册中心上的配置
+     *
+     * @return
+     * @throws SQLException
+     */
     @Bean
     public DataSource dateSource() throws SQLException {
+        Properties p = new Properties();
+        p.setProperty("sql.show", Boolean.TRUE.toString());
+        OrchestrationConfiguration orchestrationConfiguration = new OrchestrationConfiguration(
+                "sharding-sphere-orchestration", getRegistryCenterConfiguration(), true);
+
+        // 获取数据源对象
+        return OrchestrationShardingDataSourceFactory.createDataSource(
+                createDataSourceMap(), createShardingRuleConfig(), new HashMap<String, Object>(), p, orchestrationConfiguration
+        );
+    }
+
+    private Map<String, DataSource> createDataSourceMap() {
         Map<String, DataSource> dataSourceMap = new HashMap<>();
         dataSourceMap.put("ds0", createDb0());
         dataSourceMap.put("ds1", createDb1());
         dataSourceMap.put("ds2", createDb2());
         dataSourceMap.put("ds3", createDb3());
+        return dataSourceMap;
+    }
 
+    private ShardingRuleConfiguration createShardingRuleConfig() {
         ShardingRuleConfiguration shardingRuleConfig = new ShardingRuleConfiguration();
         shardingRuleConfig.getTableRuleConfigs().add(addressRuleConfig());
         shardingRuleConfig.getTableRuleConfigs().add(orderRuleConfig());
@@ -43,18 +79,23 @@ public class ShardingConfig {
         shardingRuleConfig.setDefaultDatabaseShardingStrategyConfig(
                 new StandardShardingStrategyConfiguration("user_id", new MyStandardShardingAlgorithm())
         );
+        return shardingRuleConfig;
+    }
 
-        Properties p = new Properties();
-        p.setProperty("sql.show", Boolean.TRUE.toString());
-        // 获取数据源对象
-        return ShardingDataSourceFactory.createDataSource(dataSourceMap, shardingRuleConfig, p);
+    private RegistryCenterConfiguration getRegistryCenterConfiguration() {
+        RegistryCenterConfiguration regConfig = new RegistryCenterConfiguration();
+        regConfig.setServerLists(ZOOKEEPER_CONNECTION_STRING);
+        regConfig.setNamespace(NAMESPACE);
+        return regConfig;
     }
 
     //address表分片策略
     private TableRuleConfiguration addressRuleConfig() {
         // 配置address表规则
-        TableRuleConfiguration tableRuleConfig = new TableRuleConfiguration("address_info_", "ds${0..3}.address_info_${0..9}");
-        tableRuleConfig.setKeyGeneratorConfig(new KeyGeneratorConfiguration("SNOWFLAKE", "address_id"));
+        TableRuleConfiguration tableRuleConfig = new TableRuleConfiguration();
+        tableRuleConfig.setLogicTable("address_info_");
+        tableRuleConfig.setActualDataNodes("ds${0..1}.address_info_${0..9}");
+        tableRuleConfig.setKeyGeneratorColumnName("address_id");
         //分表策略
         //自定义规则，规则比较麻烦，因此只能使用代码控制
         tableRuleConfig.setTableShardingStrategyConfig(
@@ -65,8 +106,10 @@ public class ShardingConfig {
     //order表分片策略
     private TableRuleConfiguration orderRuleConfig() {
         // 配置order表规则
-        TableRuleConfiguration tableRuleConfig = new TableRuleConfiguration("order_info_", "ds${0..3}.order_info_${0..9}");
-        tableRuleConfig.setKeyGeneratorConfig(new KeyGeneratorConfiguration("SNOWFLAKE", "order_id"));
+        TableRuleConfiguration tableRuleConfig = new TableRuleConfiguration();
+        tableRuleConfig.setLogicTable("order_info_");
+        tableRuleConfig.setActualDataNodes("ds${0..1}.order_info_${0..9}");
+        tableRuleConfig.setKeyGeneratorColumnName("order_id");
         //分表策略
         //自定义规则，规则比较麻烦，因此只能使用代码控制
         tableRuleConfig.setTableShardingStrategyConfig(
